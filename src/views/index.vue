19a5461cfb69901c4b44ac09ab917fcf50d7d3c9
冯鲁哲 <fengluzhe@corp.netease.com>
2020-09-01 14:21:24 +0800

    init

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,6 @@
 <template>
   <ul class="index">
-    首页的内容
-    <!-- <listItem :item='item' :key='i' v-for='(item, i) in list' /> -->
+    <listItem :item='item' :key='i' v-for='(item, i) in list' />
   </ul>
 </template>
 
@@ -17,14 +16,12 @@ export default {
       title: '首页'
     }
   },
-  asyncData11 ({ store, route }) {
+  asyncData ({ store, route }) {
     return store.dispatch('fetchMain', route.name)
   },
-  asyncData () {},
   computed: {
-    list11 () {
-      //return this.$store.state.items[this.$route.name].res;
-      return 111;
+    list () {
+      return this.$store.state.items[this.$route.name].res;
     }
   },
   components: {
