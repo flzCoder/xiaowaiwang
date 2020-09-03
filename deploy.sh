b86f21f0cfe236b397eb7bc693a99fad1a0ed876
冯鲁哲 <fengluzhe@corp.netease.com>
2020-09-03 15:15:19 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ BASEDIR="../home/fengluzhe"
 usage()
 {
     echo "USAGE:"
-    echo "social: 进入创业项目"
-    echo "one: 进入项目、下载github代码、重启pm2服务"
+    echo "project: 进入创业项目"
+    echo "deploy: 进入项目、下载github代码、重启pm2服务"
     exit 1
 }
-social(){
+project(){
     cd $BASEDIR"/rich"
     pwd
     ls -l
 }
-one(){
-    social;
-    ./ctrl.sh pull;
-    ./ctrl.sh restart
+deploy(){
+    project;
+    ./local.sh pull;
+    ./local.sh restart
 }
 
 if [ $# -eq 0 ]; then
