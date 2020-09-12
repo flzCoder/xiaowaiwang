91f024882039f0ef508246828d34ce066cf4c4db
冯鲁哲 <fengluzhe@corp.netease.com>
2020-09-12 13:42:28 +0800

    init

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -10,14 +10,14 @@ module.exports = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: '111111',
+    password: 'root',
     database: 'xiaowaiwang',
   },
   production:{
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: '111111',
+    password: 'root',
     database: 'xiaowaiwang',
   }
 }
