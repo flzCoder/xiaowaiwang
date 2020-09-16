a325fafd687a6e92fa3e3a6933182249583c0306
冯鲁哲 <fengluzhe@corp.netease.com>
2020-09-16 23:20:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link :to="'./message'" v-show="true"><li><span>新鲜事</span></li></router-link>
       <router-link :to="'./user'" v-show="isloged.mine"><li><span>个人主页</span></li></router-link>
       <router-link :to="'./index'" v-show="false"><li>首页</li></router-link>
-      <router-link :to="'./social'" v-show="isloged.mine"><li><span>好友</span></li></router-link>
+      <router-link :to="'./social'" v-show="true"><li><span>好友</span></li></router-link>
       <router-link :to="'./recommend'" v-show="false"><li>资讯</li></router-link>
       <router-link :to="'./info'" class="infoNav" v-show="!isloged.btn"><li><span>欢迎您:{{info.name}}</span></li></router-link>
       <router-link :to="'./login'"><li class="enroll"><span v-show="isloged.btn">登录</span><span v-show="!isloged.btn" class="logout" @click.prevent='logout'>退出</span></li></router-link>
