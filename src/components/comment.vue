0dbc7d222af7eb75f5b8da2ea31c028875fdcfae
冯鲁哲 <fengluzhe@corp.netease.com>
2020-09-22 16:47:25 +0800

    init

--- src/components/comment.vue
+++ src/components/comment.vue
@@ -5,7 +5,7 @@
       <div class="update_time" v-text="item.update_time"></div>
     </div>
     <div class="content" v-text="item.content"></div>
-    <img :src="item.pic" class="pic" alt="" />
+    <a :href="item.pic" target="_blank" v-if='item.pic'><img :src="item.pic" class="pic" alt="" /></a>
     <br>
     <div class="delbtn" v-show="item.id && info.name === item.name" @click="delComment()">删除</div>
   </li>
