45b24eb467f6a77104849e6bd958f762086382ae
fengluzhe <fengluzhe@corp.netease.com>
2021-12-15 19:32:56 +0800

    init

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.delete('/deleteMessage/:id', deleteMessageService)
 app.use(microcache.cacheSeconds(1, req => useMicroCache && req.originalUrl))
 
 function render (req, res) {
-  let host = req.hostname
+  let host = req.header.host
   console.log(666,host);
 
   const s = Date.now()
@@ -164,7 +164,6 @@ function render (req, res) {
 }
 
 app.get('*', isProd ? render : (req, res) => {
-  console.log(333,req.hostname);
   readyPromise.then(() => render(req, res))
 })
 
